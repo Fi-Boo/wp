1794fc450fbfd681fbd334ff009757366dfd6b14
Fi-Boo <s2008156@student.rmit.edu.au>
2023-01-14 00:02:09 +1100

    FINAL A2 FOR REAL

--- a2/booking.php
+++ a2/booking.php
@@ -27,7 +27,7 @@
       </ul>
     </nav>
     
-    <main>
+    <main id="main-booking">
       <section id="booking">  
         <div id="mainbox">
           <div id="movie-trailer"><iframe width="560" height="315" src="https://www.youtube.com/embed/o5F8MOz_IDw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
