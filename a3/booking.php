0f2ba65b0fb1b1ec80ca7453f423bd5cd67aae9d
Fi-Boo <s2008156@student.rmit.edu.au>
2023-01-23 12:17:35 +1100

    Copied A2 files to A3 folder.

---
+++ a3/booking.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lunardo Booking Page</title>
+    
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+    <link href="https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps:wght@700&family=Rajdhani:wght@500;700&display=swap" rel="stylesheet">
+    <script src='../wireframe.js'></script>
+  </head>
+
+  <body>
+  <header> 
+      <div id="header-bg"></div>
+      <div id="company-name">Lunardo</div> 
+    </header>
+    
+    <nav>
+      <div id="logo"><img src="../../media/logo-gold.png"></div>
+      <ul id="navbar">
+        <li>-</li>
+        <li><a href="index.php">BACK</a></li>
+        <li>-</li>
+      </ul>
+    </nav>
+    
+    <main id="main-booking">
+      <section id="booking">  
+        <div id="mainbox">
+          <div id="movie-trailer"><iframe width="560" height="315" src="https://www.youtube.com/embed/o5F8MOz_IDw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+          <div id="movie-details" >
+            <div id="movie-header">
+              <div id="movie-title">Avatar: The Way of Water</div>
+              <div id="movie-rating">PG-13</div>
+            </div>
+            <div id="movie-body">
+              <div id="movieimg"><img src="../../media/avatar2.jpg" alt="Avatar 2:Way of Water"></div>
+              <div id="movie-text">
+                <div id="movie-synopsis">Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home.</div>
+                <hr>
+                <div id="movie-director">D: James Cameron</div>
+                <hr>
+                <div id="movie-stars">C: Sam Worthington, Zoe Saldana, Sugourney Weaver</div>
+              </div>
+            </div>
+          </div>
+          
+        <div>
+      </section>
+      <section id ="booking-form">
+        <div id ="form-box">
+          <form action="booking.php?movie=ACT" method="post" onsubmit="">
+            <input type="hidden" name="movie" value="ACT">
+            
+            <!-- drop down list-->
+
+            <fieldset id="ticket-select">
+              <legend>Select your ticket(s)</legend>
+              <label for="seat[STA]">Standard Adult</label>
+            <select name="seats[STA]" data-fullprice="21.5" data-discprice="16"> 
+              <option value="">please select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select><br>
+            <label for="seat[STP]">Standard Concession</label>
+            <select name="seats[STP]" data-fullprice="19" data-discprice="14.5"> 
+              <option value="">please select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select><br>
+            <label for="seat[STC]">Standard Child</label>
+            <select name="seats[STC]" data-fullprice="17.5" data-discprice="13"> 
+              <option value="">please select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select><br>
+            <label for="seat[FCA]">First Class Adult</label>
+            <select name="seats[FCA]" data-fullprice="31.5" data-discprice="25"> 
+              <option value="">please select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select><br>
+            <label for="seat[FCP]">First Class Concession</label>
+            <select name="seats[FCP]" data-fullprice="28" data-discprice="23.5"> 
+              <option value="">please select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select><br>
+            <label for="seat[FCC]">First Class Child</label>
+            <select name="seats[FCC]" data-fullprice="25" data-discprice="22"> 
+              <option value="">please select</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select><br>
+            
+            </fieldset>
+            
+
+            <!-- radio buttons-->
+            <fieldset id="booking-day-select">
+              <legend>Select Session</legend>
+              <ul id="booking-date">
+                <li>
+                  <input type="radio" name="day" value="MON" data-pricing="discprice" required><label> Monday 9PM</label> 
+                </li>
+                <li>
+                  <input type="radio" name="day" value="TUE" data-pricing="fullprice" required><label> Tuesday 9PM</label> 
+                </li>
+                <li>
+                  <input type="radio" name="day" value="WED" data-pricing="fullprice" required><label> Wednesday 9PM</label> 
+                </li>
+                <li>
+                  <input type="radio" name="day" value="THU" data-pricing="fullprice" required><label> Thursday 9PM</label> 
+                </li>
+                <li>
+                  <input type="radio" name="day" value="FRI" data-pricing="fullprice" required><label> Friday 9PM</label> 
+                </li>
+                <li>
+                  <input type="radio" name="day" value="SAT" data-pricing="fullprice" required><label> Saturday 6PM</label> 
+                </li>
+                <li>
+                  <input type="radio" name="day" value="SUN" data-pricing="fullprice" required><label> Sunday 6PM</label> 
+                </li>
+              </ul>
+            </fieldset>
+
+            <!-- required userdata-->
+            <fieldset id="your-details">
+              <legend> Your Details </legend>
+              <label for="user[name]">Full Name: </label><input type="text" name="user[name]" required><br>
+              <label for="user[email]">Email Address: </label><input type="email" name="user[email]" required><br>
+              <label for="user[mobile]">Mobile Number (format: 04XXXXXXXX): </label><input type="tel" name="user[mobile]" pattern="04+{8}" required><br>
+              <br>
+              <input type="submit" value="Submit">
+            </fieldset>
+
+        </div>
+      </section>
+    </main>  
+
+    <footer>
+      <div>&copy;<script>
+        document.write(new Date().getFullYear());
+      </script> Phi Van Bui, s2008156D Group A. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+    </footer>
+    <aside id="debug">
+      <hr>
+      <h3>Debug Area</h3>
+      <pre>
+GET Contains:
+<?php print_r($_GET) ?>
+POST Contains:
+<?php print_r($_POST) ?>
+SESSION Contains:
+<?php print_r($_SESSION) ?>
+      </pre>
+    </aside>
+
+  </body>
+</html>
---
+++ a3/index.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lunardo Home Page</title>
+
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+    <link href="https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps:wght@700&family=Rajdhani:wght@500;700&display=swap" rel="stylesheet">
+    <script src='../wireframe.js'></script>
+  </head>
+
+  <body>
+    
+    <header> 
+      <div id="header-bg"></div>
+      <div id="company-name">Lunardo</div> 
+    </header>
+    
+    <nav>
+      <div id="logo"><img src="../../media/logo-gold.png"></div>
+      <ul id="navbar">
+        <li><a href="#aboutus">ABOUT US</a></li>
+        <li><a href="#prices">PRICES</a></li>
+        <li><a href="#nowshowing">NOW SHOWING</a></li>
+      </ul>
+    </nav>
+    
+    <main>
+      <section id="aboutus">  
+        <h2>About Us</h2>
+        <div class="aboutus-info">
+          <div class="aboutus-img"><img src="../../media/old-cinema.jpg" alt="old cinema sign"></div>
+          <div class="content">
+            <p><cite><strong>"From humble beginnings..."</strong></cite></p>
+            <p>Lunardo Cinema has been providing quality cinematic entertainment for local families since its founding in the early 1980s. As a family owned and run business, we pride ourselves on staying connected to each generation and providing a top tier personal experience to you, our customers. </p>
+            <p>The pandemic was a trying time for us, but we used that down time to focus on giving a much needed facelift to our much loved facilities. These upgrades will ensure we continue to provide the best cinematic experience for years to come.</p>
+          </div>
+        </div>
+        <h2>What's New</h2>
+        <div class="aboutus-info">
+          <div class="aboutus-img"><img src="../../media/upgraded-seating.jpg" alt="upgraded seating image"></div>
+          <div class="content">
+            <p><cite><strong>"Comfort and luxury while you watch..."</strong></cite></p>
+            <p>All of our seating has been upgraded to meet the demands of the modern cinematic experience. These features include:</p>
+            <ul>
+              <li>110 degree recline</li>
+              <li>flip up armrest</li>
+              <li>inbuilt cupholders</li>
+              <li>Leather trimmed seating (First Class only)</li>
+            </ul>
+          </div>
+        </div>
+        <div class="aboutus-info"> 
+          <div class="aboutus-img"><img src="../../media/dolby.png" alt="dolby logos"></div>
+          <div class="content">
+            <p><cite><strong>"A truly immersive experience..."</strong></cite><p>
+            <p>All cinema room projectors and sound systems have been upgraded with 3D Dolby Vision projection and Dobly Atomos sound to offer the best viewing experience for modern titles.</p>
+          </div>
+        </div>   
+      </section>
+
+      <section id="prices">
+        <h2> Prices </h2>
+        <div class="box">
+          <div class="innerbox">
+            <div class="seating">
+              <div class="seatinfo-top">
+                <div id="std-class"><p>Standard</p></div>
+                <div class="seatimg"><img src='../../media/Profern-Standard-Twin.png' alt='Standard Seat image'></div>
+              </div>
+              <div class ="seatinfo">
+                <p>The Profurn 9X8 seat is designed with a distinct headrest to improve acoustics and the sound experience without compromising on comfort or aesthetic.</p>
+                <p>The 9X8 seat has retractable armrests and includes low level cup holders.</p>
+              </div>
+            </div>
+            <div class="seating">
+              <div class="seatinfo-top">
+                <div id="first-class"><p>First Class</p></div>
+                <div class="seatimg"><img src='../../media/Profern-Verona-Twin.png' alt='First Class Seat image'></div>
+              </div>
+              <div class ="seatinfo">
+                <p>The Verona seat is designed for the ultimate in first class seating with it's plush leather trim and 110 degree recliner function.</p>
+                <p>The Verona seat has 110 degree recliner and leg rests and large swivel table.</p> 
+              </div>
+            </div>  
+            <table id="pricestable">
+              <tr>
+                <th class="nu">Ticket</th>
+                <th class="st">Standard</th>
+                <th class="std">*</th>
+                <th class="fc">First Class</th>
+                <th class="fcd">*</th>
+              </tr>
+              <tr>
+                <td class="nu">Adult</td>
+                <td class="st">$21.50</td>
+                <td class="std">$16.00</td>
+                <td class="fc">$31.50</td>
+                <td class="fcd">$25.00</td>
+              </tr>
+              <tr>
+                <td class="nu">Concession</td>
+                <td class="st">$19.00</td>
+                <td class="std">$14.50</td>
+                <td class="fc">$28.00</td>
+                <td class="fcd">$23.50</td>
+              </tr>
+              <tr>
+                <td class="nu">Child</td>
+                <td class="st">$17.50</td>
+                <td class="std">$13.00</td>
+                <td class="fc">$25.00</td>
+                <td class="fcd">$22.00</td>
+              </tr>
+            </table>
+          </div>
+        </div>  
+        <cite><strong>*Daily 12pm session and all day Monday. </strong></cite>
+      </section>
+
+      <section id="nowshowing">
+        <h2>Now Showing</h2>
+        <div id="movie-section">
+          <div class="movie-single">
+            <div class="movie-detail">
+              <div class="test"></div>
+              <div class="movie-title">Avatar 2: Way of water</div>
+              <div class="movie-rating">PG-13</div>
+            </div>
+            <div class="flip-card-container">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="card-front">
+                    <img src="../../media/avatar2.jpg">
+                  </div>
+                  <div class="card-back">
+                    <p>Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home.</p>
+    
+                    <cite><strong>Session Times</strong></cite>
+                    <table>
+                      <tr>
+                        <th>Mon-Tue</th>
+                        <th>Wed-Fri</th>
+                        <th>Sat-Sun</th>
+                      </tr>
+                      <tr>
+                        <td>9pm</td>
+                        <td>9pm</td>
+                        <td>6pm</td>
+                      </tr>
+                    </table>
+                    <div class="booknow">
+                      <a href="booking.php?movie=ACT">BOOK NOW</a>
+                    </div>
+                  </div>
+                </div>
+              </div>    
+            </div>
+          </div>
+
+          <div class="movie-single">
+            <div class="movie-detail">
+              <div class="movie-title">Weird: The Al Yankovic Story</div>
+              <div class="movie-rating">TV-14</div>
+            </div>
+            <div class="flip-card-container">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="card-front">
+                    <img src="../../media/weird.jpg">
+                  </div>
+                  <div class="card-back">
+                    <p>Explores every facet of Yankovic's life, from his meteoric rise to fame with early hits like 'Eat It' and 'Like a Surgeon' to his torrid celebrity love affairs and famously depraved lifestyle.</p>
+    
+                    <cite><strong>Session Times</strong></cite>
+                    <table>
+                      <tr>
+                        <th>Mon-Tue</th>
+                        <th>Wed-Fri</th>
+                        <th>Sat-Sun</th>
+                      </tr>
+                      <tr>
+                        <td>-</td>
+                        <td>12pm</td>
+                        <td>3pm</td>
+                      </tr>
+                    </table>
+                    <div class="booknow">
+                      <a href="booking.php?movie=RMC">BOOK NOW</a>
+                    </div>
+                  </div>
+                </div>    
+              </div>
+            </div>
+          </div>
+
+          <div class="movie-single">
+            <div class="movie-detail">
+              <div class="movie-title">Puss in Boots: The Last Wish</div>
+              <div class="movie-rating">PG</div>
+            </div>
+            <div class="flip-card-container">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="card-front">
+                    <img src="../../media/pussinboots.jpg">
+                  </div>
+                  <div class="card-back">
+                    <p>Puss in Boots discovers that his passion for adventure has taken its toll: he has burned through eight of his nine lives. Puss sets out on an epic journey to find the mythical Last Wish and restore his nine lives.</p>
+    
+                    <cite><strong>Session Times</strong></cite>
+                    <table>
+                      <tr>
+                        <th>Mon-Tue</th>
+                        <th>Wed-Fri</th>
+                        <th>Sat-Sun</th>
+                      </tr>
+                      <tr>
+                        <td>12pm</td>
+                        <td>6pm</td>
+                        <td>12pm</td>
+                      </tr>
+                    </table>
+                    <div class="booknow">
+                      <a href="booking.php?movie=FAM">BOOK NOW</a>
+                    </div>
+                  </div>
+                </div>    
+              </div>
+            </div>
+          </div>
+
+          <div class="movie-single">
+            <div class="movie-detail">
+              <div class="movie-title">Margrete: Queen of the North</div>
+              <div class="movie-rating">MA-15+</div>
+            </div>
+            <div class="flip-card-container">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="card-front">
+                    <img src="../../media/margrete.jpg">
+                  </div>
+                  <div class="card-back">
+                    <p>1402. Queen Margrete is ruling Sweden, Norway and Denmark through her adopted son, Erik. But a conspiracy is in the making and Margrete finds herself in an impossible dilemma that could shatter her life's work: the Kalmar Union.</p>
+    
+                    <cite><strong>Session Times</strong></cite>
+                    <table>
+                      <tr>
+                        <th>Mon-Tue</th>
+                        <th>Wed-Fri</th>
+                        <th>Sat-Sun</th>
+                      </tr>
+                      <tr>
+                        <td>6pm</td>
+                        <td>-</td>
+                        <td>10pm</td>
+                      </tr>
+                    </table>
+                    <div class="booknow">
+                      <a href="booking.php?movie=AHF">BOOK NOW</a>
+                    </div>
+                  </div>
+                </div>    
+              </div>
+            </div>
+          </div>
+        </div>  
+      </section>
+    </main>
+
+    <footer>
+      <div>&copy;<script>
+        document.write(new Date().getFullYear());
+      </script> Phi Van Bui, s2008156D Group A. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+    </footer>
+  </body>
+</html>
