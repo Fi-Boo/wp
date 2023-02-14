defe9da8184b42131014dd613a618f3ca84c6897
Fi-Boo <s2008156@student.rmit.edu.au>
2023-02-14 14:45:37 +1100

    re-enabled session blocking

--- a3test/tools.php
+++ a3test/tools.php
@@ -723,10 +723,10 @@ function sessionSelection($var) {
           $pricing = "data-fullprice";
         }
 
-        // disables radio if no session time  = "-"
-        // if ($time == "-") {
-        //   $radioState = "disabled";
-        // } 
+        disables radio if no session time  = "-"
+        if ($time == "-") {
+          $radioState = "disabled";
+        } 
 
         $checkedState = setChecked($_POST['day'], $day);
     
